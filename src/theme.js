d90ef07f7e7f85c0161b302c85177eefa5552ce6
uknmr <y.kinmori@gmail.com>
2016-08-28 17:11:47 +0900

    ➕ デフォルトのMaterial design themeの読み込みファイルを追加。
    明示的に読み込まないと初回に反映されなかったため。

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import {
+  theme,
+  setTheme,
+  MKColor
+} from 'react-native-material-kit';
+
+setTheme({
+  primaryColor: MKColor.BlueGrey,
+  primaryColorRGB: MKColor.RGBBlueGrey,
+  accentColor: MKColor.Amber,
+  accentColorRGB: MKColor.RGBAmber,
+});
+
+export default theme;
