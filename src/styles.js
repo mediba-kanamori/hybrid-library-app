8932f18262df844d33f9edf1d090fdf91d329b77
uknmr <y.kinmori@gmail.com>
2016-08-29 02:59:08 +0900

    ➕ Stylesを追加

---
+++ src/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  listView: {
+    flex: 1
+  },
+  liContainer: {
+    flex: 2,
+  },
+  liText: {
+    color: '#333',
+    fontSize: 16
+  },
+  navbar: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 44,
+    flexDirection: 'row'
+  },
+  navbarTitle: {
+    color: '#444',
+    fontSize: 16,
+    fontWeight: '500'
+  },
+  statusBar: {
+    height: 22
+  }
+});
+
+export default styles;
