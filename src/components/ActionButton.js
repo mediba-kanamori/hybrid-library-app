54ba149d66a453f4028c2dd2c41ab1a9d0b7e6fd
uknmr <y.kinmori@gmail.com>
2016-08-31 00:12:55 +0900

    ♻️ FABをガイドラインに沿ってスタイリング

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { MKButton } from 'react-native-material-kit';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from '../styles';
 
 const FloatingActionButton = MKButton.coloredFab()
   .withAccent(true)
+  .withStyle({
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+  })
   .withOnPress(() => {
     console.log("Hi, it's a colored button!");
   })
@@ -13,7 +22,7 @@ class ActionButton extends Component {
   render() {
     return (
       <FloatingActionButton onPress={this.props.onPress}>
-        <Icon name="cart-plus" size={30} color="white" />
+        <Icon name="cart-plus" size={24} color="white" />
       </FloatingActionButton>
     );
   }
--- src/styles.js
+++ src/styles.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   },
   statusBar: {
     height: 22
-  }
+  },
 });
 
 export default styles;
